6dfe654a78d0ad141c7bd1fe568bba582d70cf50
Ritik <ritik.sharma.averybit@gmail.com>
2025-10-15 18:27:43 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -61,7 +61,9 @@ app.add_middleware(
     allow_methods=["*"], allow_headers=["*"],
 )
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
 
 @app.get("/")
 async def home():
